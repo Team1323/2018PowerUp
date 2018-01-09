b68c9ad6dfd942e9f4659a1fc834186b09bc0cdb
Team1323 <madtownrobotics@maderausd.org>
2018-01-08 20:55:18 -0800

    adding structural classes

---
+++ src/com/team1323/frc2018/subsystems/Subsystem.java
@@ -0,0 +1,9 @@
+package com.team1323.frc2018.subsystems;
+
+public abstract class Subsystem {
+    public abstract void outputToSmartDashboard();
+
+    public abstract void stop();
+
+    public abstract void zeroSensors();
+}
