fcfdcd6effbb6cc07371cfa21b2725ae802001ad
Joseph Reed <josephreed2600@gmail.com>
2018-05-07 23:54:33 -0700

    Update RadialVectorField.java
    Implement Translation2d

--- src/com/team1323/frc2018/vectors/RadialVectorField.java
+++ src/com/team1323/frc2018/vectors/RadialVectorField.java
@@ -3,13 +3,13 @@
 
 public class RadialVectorField implements VectorField {
 	// Default direction is toward point
-	public RadialVectorField(double x, double y) {
-		x_ = x;
-		y_ = y;
+	public RadialVectorField(Translation2d where) {
+		x_ = where.x();
+		y_ = where.y();
 	}
-	public RadialVectorField(double x, double y, bool away) {
-		x_ = x;
-		y_ = y;
+	public RadialVectorField(Translation2d where, boolean away) {
+		x_ = where.x();
+		y_ = where.y();
 		if(away) direction = -1;
 	}
 	
@@ -17,16 +17,8 @@ public class RadialVectorField implements VectorField {
 	protected double x_;
 	protected double y_;
 	
-	public double[] getVector(double x, double y) {
-		double dx = x_ - x;
-		double dy = y_ - y;
-		double m = Math.sqrt(dx*dx+dy*dy);
-		return new double[]{dx/m,dy/m};
+	public Translation2d getVector(double x, double y) {
+		Translation2d v = new Translation2d(x_-x,y_-y);
+		return v.scale(1/v.norm());
 	}
-	public double getX(double x, double y) { return getVector(x,y)[0]; }
-	public double getY(double x, double y) { return getVector(x,y)[1]; }
-/*	public Translation2d getTranslation2d(double x, double y) {
-		double[] v = getVector(x,y);
-		return new Translation2d(v[0],v[1]);
-	} */
 }
