4c6d69e3043d45c11917d6cd6cdee61e8a55affb
Joseph Reed <josephreed2600@gmail.com>
2018-05-07 23:46:53 -0700

    Update VectorField.java
    Implement Translation2d and realize that's really the only thing we need to do to interact with this class

--- src/com/team1323/frc2018/vectors/VectorField.java
+++ src/com/team1323/frc2018/vectors/VectorField.java
@@ -2,8 +2,5 @@
 //import com.team254.lib.util.math;
 
 public interface VectorField {
-	public abstract double[] getVector(double x, double y);
-	public abstract double getX(double x, double y);
-	public abstract double getY(double x, double y);
-/*	public abstract Translation2d getTranslation2d(double x, double y); */
+	public abstract Translation2d getVector(Translation2d here);
 }
