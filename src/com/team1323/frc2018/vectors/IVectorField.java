703d16dea2ac6d19977fc935ad34583ea72c1250
Joseph Reed <josephreed2600@gmail.com>
2018-05-10 10:45:21 -0700

    Update and rename VectorField.java to IVectorField.java

--- src/com/team1323/frc2018/vectors/IVectorField.java
+++ src/com/team1323/frc2018/vectors/IVectorField.java
@@ -1,6 +1,6 @@
 //package com.team1323.reee;
 //import com.team254.lib.util.math;
 
-public interface VectorField {
+public interface IVectorField {
 	public abstract Translation2d getVector(Translation2d here);
 }
