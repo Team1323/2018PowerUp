15c1d60a587096b4c280bee4479986825a51266a
Joseph Reed <josephreed2600@gmail.com>
2018-05-10 10:59:24 -0700

    Update ConstantVectorField.java
    IVectorField and normalize on construction

--- src/com/team1323/frc2018/vectors/ConstantVectorField.java
+++ src/com/team1323/frc2018/vectors/ConstantVectorField.java
@@ -1,9 +1,9 @@
 //package com.team1323.reee;
 //import com.team254.lib.util.math;
 
-public class ConstantVectorField implements VectorField {
+public class ConstantVectorField implements IVectorField {
 	public ConstantVectorField(Translation2d whichWay) {
-		thatWay = whichWay;
+		thatWay = whichWay.scale(1/whichWay.norm());
 	}
 	
 	protected Translation2d thatWay;
